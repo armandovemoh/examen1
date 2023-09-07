911f2781d48d16e7ed017c45e91f0b3b018145ae
armandovemoh <141792435+armandovemoh@users.noreply.github.com>
2023-09-07 10:32:42 -0600

    Add files via upload

--- Ejecucion.java
+++ Ejecucion.java
@@ -1,11 +1,17 @@
 package examenparcial;
+import java.util.Scanner;
 
 public class Ejecucion {
 
-	public void IniciarPrograma() {
-		Palabras palabras = new Palabras("");
+    public void IniciarPrograma() {
+        Scanner scanner = new Scanner(System.in);
         Extra extra = new Extra();
-		
-	}
+
+        boolean ejecutarFizzBuzz = extra.preguntarEjecucionFizzBuzz();
+
+        if (ejecutarFizzBuzz) {
+            extra.fizzBuzz();
+        }
+    }
 
 }
--- Extra.java
+++ Extra.java
@@ -1,5 +1,25 @@
 package examenparcial;
+import java.util.Scanner;
 
 public class Extra {
+    public void fizzBuzz() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 
+    public boolean preguntarEjecucionFizzBuzz() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("¿Desea ejecutar FizzBuzz? (si(S)/no(N)): ");
+        String respuesta = scanner.nextLine();
+        return respuesta.equalsIgnoreCase("S");
+    }
 }
